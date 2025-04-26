4efd3d6c41e6f509ad943a720b6701ddf4458b4c
issei <proteinebimayoduu@gmail.com>
2025-04-26 16:30:09 +0900

    データベースの値を表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
     # デフォルトで何も書かなければ views/home/index.html.erbを表示する
-    @title = 'デイトラ'
+     @article = Article.first
     end
 
     def about
