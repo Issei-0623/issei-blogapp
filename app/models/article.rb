cd7429ac5def7f22fad9d35b72ecd590e80ed921
issei <proteinebimayoduu@gmail.com>
2025-05-18 11:30:03 +0900

    ログイン必須のアクションを指定 erb→hamlファイル変換など行う

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :title, length: { minimum: 2, maximum: 20 }
+    validates :title, format: { with: /\A(?!@)/ }
+
+    validates :content, presence: true
+    validates :content, length: { minimum: 10 }
+    validates :content, uniqueness: true
+
+    validate :validate_title_and_content_length
+
+    def display_created_at
+        I18n.l(self.created_at, format: :default)
+    end
+
+    private
+    def validate_title_and_content_length
+        char_count = self.title.length + self.content.length
+        unless char_count > 100
+            errors.add(:content, '100文字以上で！')
+        end
+    end
 end
